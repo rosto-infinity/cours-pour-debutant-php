bb6991ce2cccc5e7db71735989fc261b63b1b0bf
rosto-infinity <lelewaffo@gmail.com>
2022-01-23 12:45:01 +0100

    introduction

---
+++ 1-introduction.php
@@ -0,0 +1,9 @@
+/**
+
+ Apprendre PHP
+PHP est un langage de script serveur et un outil puissant pour créer des
+pages Web dynamiques et interactives.
+PHP est une alternative largement utilisée, gratuite et efficace à des
+concurrents tels que l'ASP de Microsoft.
+
+*/
